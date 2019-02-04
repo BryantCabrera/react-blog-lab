8887b64559037f83ff640767c449e58d52464739
Bryant <cabrera.bryant@gmail.com>
2019-02-04 14:20:18 -0800

    Implements State.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import Post from './Post';
 
 class App extends Component {
   state = {
+    title: 'Harry Potter and the Goblet of Fire',
+    author: 'J.K. Rowling',
+    body: 'The 4th installment in the Harry Potter series.',
+    comments: ['Great read!', 'Amazing.', 'Way too pre-teeny.']
+  }
 
+  handleClick = () => {
+    console.log(' handled click');
   }
 
   render() {
+    const {title, author, body, comments} = this.state;
     return (
       <div className="App">
-        <Post title={'Harry Potter and the Goblet of Fire'} author={'J.K. Rowling'} body={'The 4th installment in the Harry Potter series.'} comments={['Great read!', 'Amazing.', 'Way too pre-teeny.']}/>
+        <Post title={title} author={author} body={body} comments={comments} handleClick={this.handleClick}/>
       </div>
     );
   }
--- src/Post.js
+++ src/Post.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import Comment from './Comment.js'
 
-class Post extends Component {
-    render () {
-        return (
+// class Post extends Component {
+const Post = ({title, author, body, comments, handleClick}) => {
+    // constructor () {
+    //     super();
+    //     // this.state = {
+    //     //     body: 
+    //     // }
+    // }
+
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>By {author}</p>
             <div>
-                <h1>{this.props.title}</h1>
-                <p>By {this.props.author}</p>
-                <div>
-                    <p>{this.props.body}</p>
-                </div>
-                <h3>Comments:</h3>
-                <ul>
-                    {this.props.comments.map ((comment, index) => <li><Comment body={comment} /></li>)}
-                </ul>   
+                <p>{body}</p>
             </div>
-        );
-    }
+            <h3>Comments:</h3>
+            <ul>
+                {comments.map ((comment, index) => <li><Comment body={comment} /></li>)}
+            </ul>  
+            <button onClick={handleClick}>Handle Click</button> 
+        </div>
+    );
 }
 
 export default Post;
